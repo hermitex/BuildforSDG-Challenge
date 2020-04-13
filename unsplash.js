048adfef41837df9aaa629805136e0e57f191070
Tiberius <mercytiberius@yahoo.com>
2020-04-13 12:14:32 +0300

    change structure

--- unsplash.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable indent */
-/* eslint-disable linebreak-style */
-/* eslint-disable padded-blocks */
-// const addIndex = () => document.querySelector('#form-container').classList.add('addIndex');
-// setTimeout(() => {
-//     // addIndex();
-//     document.querySelector('#splash').classList.add('fade');
-
-// }, 2000);
